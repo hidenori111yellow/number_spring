bcc92968c04a752d241de193764436080c71dcef
Hidenori Yabuuchi <hidenori111yellow@icloud.com>
2019-08-08 13:18:56 +0900

    iOS/Androidダイアログスタイルの変更機能追加

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:number_spring/utils/share_preferece.dart';
 import 'package:number_spring/main.dart';
 
+final appDrawerStateKey = GlobalKey<AppDrawerState>();
+
 class AppDrawer extends StatefulWidget {
   @override
-  _AppDrawerState createState() => _AppDrawerState();
+  AppDrawerState createState() => AppDrawerState();
 }
 
-class _AppDrawerState extends State<AppDrawer> {
+class AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -19,34 +22,78 @@ class _AppDrawerState extends State<AppDrawer> {
           ListTile(
             title: Text('再スタート'),
             onTap: (){
-              showDialog(context: context,
-              builder: (BuildContext context){
-                return AlertDialog(
-                  content: Text('現在のゲームをリセットしてもよろしいですか？'),
-                  actions: <Widget>[
-                    FlatButton(child: Text('いいえ'),onPressed: (){
-                      Navigator.pop(context);
-                    },),
-                    FlatButton(child: Text('はい'),onPressed: (){
-                      Stopwatch sw = new Stopwatch();
-                      sw.start();
-                      for(int i = 1; i <= 15; i++){
-                        if(!SharedPref.getBall(i)){
-                        SharedPref.setBall(i, true);                          
+              if(SharedPref.getAndroidStyle()){
+                showDialog(context: context,
+                builder: (BuildContext context){
+                  return AlertDialog(
+                    content: Text('現在のゲームをリセットしてもよろしいですか？'),
+                    actions: <Widget>[
+                      FlatButton(child: Text('いいえ'),onPressed: (){
+                        Navigator.pop(context);
+                      },),
+                      FlatButton(child: Text('はい'),onPressed: (){
+                        for(int i = 1; i <= 15; i++){
+                          if(!SharedPref.getBall(i)){
+                          SharedPref.setBall(i, true);                          
+                          }
                         }
-                      }
-                      sw.stop();
-                      
-                      print('${sw.elapsedTicks} ms');                      
-                      
-                      myAppKey.currentState.setState((){});
-                      
-                      Navigator.pop(context);
-                    },),
-                  ],
+                        myAppKey.currentState.setState((){});
+
+                        Navigator.pop(context);
+                      },),
+                    ],
                   );
-              },
+                },
               );
+              }else{
+                showDialog(context: context,
+                builder: (BuildContext context){
+                  return CupertinoAlertDialog(
+                    content: Text('現在のゲームをリセットしてもよろしいですか？'),
+                    actions: <Widget>[
+                      FlatButton(child: Text('いいえ'),onPressed: (){
+                        Navigator.pop(context);
+                      },),
+                      FlatButton(child: Text('はい'),onPressed: (){
+                        for(int i = 1; i <= 15; i++){
+                          if(!SharedPref.getBall(i)){
+                          SharedPref.setBall(i, true);                          
+                          }
+                        }
+                        myAppKey.currentState.setState((){});
+
+                        Navigator.pop(context);
+                      },),
+                    ],
+                  );
+                },
+              );
+              }
+            },
+          ),
+          SwitchListTile(
+            // secondary: Row(
+            //   children: <Widget>[
+            //     Icon(Icons.phone_iphone),
+            //     Icon(Icons.phone_android),
+            //   ],
+            // ),
+            title: Row(
+              children: <Widget>[
+                Icon(Icons.phone_iphone),
+                Padding(
+                  padding: const EdgeInsets.only(right:8.0),
+                  child: Icon(Icons.phone_android),
+                ),
+                Text('iOS/Android'),
+              ],
+            ),
+            value: SharedPref.getAndroidStyle(),
+            onChanged: (bool value) async{
+              SharedPref.setAndroidStyle(value);
+              
+              myAppKey.currentState.setState((){});
+              
             },
           ),
         ],
